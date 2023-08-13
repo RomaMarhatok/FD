13063ad47f80e2c639ae00ea6175731675fab747
roma <roma.marhatok@yandex.by>
2023-08-13 19:36:19 +0300

    change type for comparing with db model

--- app/domain/person/characteristics/special_sign.py
+++ app/domain/person/characteristics/special_sign.py
@@ -1,8 +1,7 @@
-import uuid
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(kw_only=True)
 class SpecialSign:
+    ref: str
     description: str
-    ref: uuid.UUID = field(default_factory=uuid.uuid4)
