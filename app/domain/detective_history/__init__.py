c769ae8014f258ce289c7f2a5350135916b0e7bd
roma <roma.marhatok@yandex.by>
2023-08-13 14:12:14 +0300

    update detective history domain model

--- app/domain/detective_history/__init__.py
+++ app/domain/detective_history/__init__.py
@@ -1,16 +1,21 @@
+import uuid
+from dataclasses import field
 import member as history_member
 import status
+import clue
+import proofs
 
 
 class DetectiveHistory:
-    clues: list[str]  # rework
-    proofs: list[str]  # rework
+    clues: set[clue.Clue]
+    proofs: set[proofs.Proofs]
     incident_description: str
-    members: list[history_member.DetectiveHistoryMember]
+    members: set[history_member.DetectiveHistoryMember]
     answer: history_member.DetectiveHistoryMember
+    ref: uuid.UUID = field(default_factory=uuid.uuid4)
 
     @property
-    def suspect(self) -> list:
+    def suspect(self) -> list[history_member.DetectiveHistoryMember]:
         return [
             member
             for member in self.members
