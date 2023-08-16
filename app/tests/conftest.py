d61c458766d86a8e99e61dd81b95677dc7716bbc
roma <roma.marhatok@yandex.by>
2023-08-16 18:21:30 +0300

    conftest refactoring

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,12 +1,18 @@
 import os
+import asyncio
+import sys
 import pytest
-
-from contextlib import contextmanager
+import pytest_asyncio
 from yarl import URL
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from alembic.command import upgrade
 from app.db.utils import temp_database, alembic_config_from_url
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 @pytest.fixture
@@ -26,21 +32,6 @@ def temp_pg_database(pg_url):
         yield db_urL
 
 
-@contextmanager
-def init_engine(db_url, factory=None):
-    try:
-        engine = factory(db_url, echo=True)
-        yield engine
-    finally:
-        engine.dispose()
-
-
-@pytest.fixture
-def pg_sync_engine(temp_pg_database):
-    with init_engine(temp_pg_database, factory=create_engine) as engine:
-        yield engine
-
-
 @pytest.fixture
 def migrated_temp_pg_database(temp_pg_database):
     config = alembic_config_from_url(temp_pg_database)
@@ -49,6 +40,22 @@ def migrated_temp_pg_database(temp_pg_database):
 
 
 @pytest.fixture
-def migrated_pg_sync_engine(migrated_temp_pg_database):
-    with init_engine(migrated_temp_pg_database, factory=create_engine) as engine:
-        yield engine
+def migrated_pg_sync_session(migrated_temp_pg_database) -> Session:
+    engine = create_engine(migrated_temp_pg_database)
+    try:
+        session_factory = sessionmaker(engine, expire_on_commit=False)
+        session: Session = session_factory()
+        yield session
+    finally:
+        session.close()
+
+
+@pytest_asyncio.fixture
+async def migrated_pg_async_session(migrated_temp_pg_database) -> AsyncSession:
+    engine = create_async_engine(migrated_temp_pg_database, echo=True)
+    session_factory = async_sessionmaker(engine, expire_on_commit=False)
+    try:
+        async_session = session_factory()
+        yield async_session
+    finally:
+        await async_session.close()
--- app/tests/integration/test_orm/conftest.py
+++ app/tests/integration/test_orm/conftest.py
@@ -1,26 +1,5 @@
-import pytest
 from sqlalchemy import text
-from sqlalchemy.orm import sessionmaker, Session
-
-
-@pytest.fixture
-def pg_sync_session(pg_sync_engine) -> Session:
-    try:
-        session: Session = sessionmaker(pg_sync_engine, expire_on_commit=False)()
-        yield session
-    finally:
-        session.close()
-
-
-@pytest.fixture
-def migrated_pg_sync_session(migrated_pg_sync_engine) -> Session:
-    try:
-        session: Session = sessionmaker(
-            migrated_pg_sync_engine, expire_on_commit=False
-        )()
-        yield session
-    finally:
-        session.close()
+from sqlalchemy.orm import Session
 
 
 def insert_characteristics(
